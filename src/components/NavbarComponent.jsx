51d2f8cb7e56a61633a6146af48106e522efcf34
ronin-developer <milan@outlook.at>
2024-03-26 23:57:33 +0100

    first commit

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -6,7 +6,7 @@ function NavbarComponent() {
 
   return (
     <div className='container mx-auto flex items-center justify-between h2-[120px]'>
-        <div>
+        <div className='flex justify-between items-center'>
             logo
         </div>
         <ul>
